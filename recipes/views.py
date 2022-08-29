260d0bfd37238ca3856ff4e9d2d1bb156b81318e
Efraim Kristhianno <ekriator@gmail.com>
2022-08-29 17:24:09 -0300

    atualizando informaçoes

--- recipes/views.py
+++ recipes/views.py
@@ -4,15 +4,18 @@ from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
@@ -22,4 +25,3 @@ def recipe(request, id):
         'recipe': make_recipe(),
         'is_detail_page': True,
     })
-
