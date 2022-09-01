8125362780a1c29a6450a2725707225a145e4492
Efraim Kristhianno <ekriator@gmail.com>
2022-09-01 14:13:56 -0300

    usando o if e o http404 para requisiçoes nao encontradas

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
@@ -19,7 +19,7 @@ def category(request, category_id):
         category__id=category_id, is_published=True).order_by('-id')
 
     if not recipes:
-        return HttpResponse(content='Not found', status=404)
+        raise Http404('Not found 😢🤔🚀')
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
