a61487157a4e1795659536d7a43a7f1a6322cabc
Efraim Kristhianno <ekriator@gmail.com>
2022-09-01 14:04:22 -0300

    Usando o HttpResponse para respostas de requisição

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
+
 from recipes.models import Recipe
 
 
@@ -14,8 +17,13 @@ def home(request):
 def category(request, category_id):
     recipes = Recipe.objects.filter(
         category__id=category_id, is_published=True).order_by('-id')
+
+    if not recipes:
+        return HttpResponse(content='Not found', status=404)
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category |'
     })
 
 
