0221192c092945dd59906d314e45f44bcaa8350b
Efraim Kristhianno <ekriator@gmail.com>
2022-09-01 14:40:07 -0300

    organizando a identação

--- recipes/views.py
+++ recipes/views.py
@@ -6,17 +6,21 @@ from recipes.models import Recipe
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={'recipes': recipes, })
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes, })
 
 
 def category(request, category_id):
-    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id,
-                                                    is_published=True,).order_by('-id'))
-
-    return render(request, 'recipes/pages/category.html', context={'recipes': recipes,
-                                                                   'title': f'{recipes[0].category.name} - Category |'})
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id, is_published=True,
+    ).order_by('-id'))
+    return render(request, 'recipes/pages/category.html',
+                  context={'recipes': recipes,
+                           'title': f'{recipes[0].category.name}- Category |'
+                           })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', context={'recipe': make_recipe(),
-                                                                      'is_detail_page': True, })
+    return render(request, 'recipes/pages/recipe-view.html',
+                  context={'recipe': make_recipe(),
+                           'is_detail_page': True, })
