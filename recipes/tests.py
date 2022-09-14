a2eac8c27954a73476cfcdf5669a2601b92b54f8
Efraim Kristhianno <ekriator@gmail.com>
2022-09-14 17:00:03 -0300

    working with pytest united

--- recipes/tests.py
+++ recipes/tests.py
@@ -3,4 +3,5 @@ from django.test import TestCase
 
 class RecipeURLsTest(TestCase):
     def test_the_pytest_is_ok(self):
-        ...
+        print('Ola mundo')
+        assert 1 == 1, 'um é igual a um mesmo'
