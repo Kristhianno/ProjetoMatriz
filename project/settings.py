a3053a18b488514a83de89a1aa23c6e2fc84db17
Efraim Kristhianno <ekriator@gmail.com>
2022-09-14 15:01:28 -0300

    aprendendo a ignorar erros especificos de flake8

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bwnr5ex97ngh!(5+j=1d9liy&=mtz+=3x#!($$y6tr+monjo07'
+SECRET_KEY = 'django-insecure-bwnr5ex97ngh!(5+j=1d9liy&=mtz+=3x#!($$y6tr+monjo07'  # noqa E501
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
